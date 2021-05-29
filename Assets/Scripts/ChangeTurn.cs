953132bae71d3a40a8d882534a611e2ae9f91e28
Feargin <feargin@gmail.com>
2021-05-29 13:59:03 +0500

    !F spawn enemy & turns

--- Assets/Scripts/ChangeTurn.cs
+++ Assets/Scripts/ChangeTurn.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeTurn : MonoBehaviour
 {
+    [SerializeField] private Button _nextTurn;
     
+    #region Events
+    public static event System.Action<bool> TheNextTurn;
+    #endregion
     void Start()
     {
         
@@ -12,7 +17,16 @@ public class ChangeTurn : MonoBehaviour
 
     public void NextTurn()
     {
-        
+        _nextTurn.interactable = false;
+        Spawn.Instance.PlayerControler.GetComponent<PlayerMovement>().enabled = false;
+        TheNextTurn?.Invoke(true);
+    }
+
+    public void FinishEnemyTurn()
+    {
+        _nextTurn.interactable = true;
+        Spawn.Instance.PlayerControler.GetComponent<PlayerMovement>().enabled = true;
+        TheNextTurn?.Invoke(false);
     }
     void Update()
     {
